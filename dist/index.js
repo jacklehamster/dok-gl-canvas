d9620a3f7feb527c7bbb3a8eab40bbe1750491e6
Jack Le Hamster <jacklehamster@gmail.com>
2023-07-04 18:18:26 -0700

    cr

--- dist/index.js
+++ dist/index.js
@@ -26828,7 +26828,7 @@ var ReactHook = /*#__PURE__*/function () {
   ReactHook.hookup = function hookup(hud, Node, props, controller) {
     reactDom.render(React__default.createElement(Control, {
       controller: controller
-    }, React__default.createElement(Node, Object.assign({}, props))), hud);
+    }, React__default.createElement(Node, _extends({}, props))), hud);
   };
   return ReactHook;
 }();
