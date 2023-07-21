e2e9492d6b3d04cd04305922247e08d26f87709f
Jack Le Hamster <jacklehamster@gmail.com>
2023-07-21 14:30:01 -0700

    np

--- dist/control/gl-controller.d.ts
+++ dist/control/gl-controller.d.ts
@@ -1,11 +1,11 @@
 import { ExecutionParameters, ProgramId } from "dok-gl-actions";
-export declare type GetUniformLocation = (name: string, id?: ProgramId) => WebGLUniformLocation | undefined;
-export declare type GetAttributeLocation = (name: string, id?: ProgramId) => number;
+export type GetUniformLocation = (name: string, id?: ProgramId) => WebGLUniformLocation | undefined;
+export type GetAttributeLocation = (name: string, id?: ProgramId) => number;
 export interface GlConfig {
     gl: WebGL2RenderingContext;
     getUniformLocation: GetUniformLocation;
     getAttributeLocation: GetAttributeLocation;
 }
 export interface GlController {
-    executeScript?(name: string, parameters?: ExecutionParameters): void;
+    executeScript?(name: string, parameters?: ExecutionParameters): Promise<() => void>;
 }
