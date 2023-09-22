3e8f8163499bd59f127175e5c34ab103a329b2e1
Jack Le Hamster <jacklehamster@gmail.com>
2023-09-23 05:17:09 +0900

    pieces

--- dist/ReactHook.d.ts
+++ dist/ReactHook.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="react" />
 import { Controller } from "./control/controller";
 export default class ReactHook {
     static hookup<Props>(hud: HTMLDivElement, Node: () => JSX.Element, props?: Props, controller?: Controller): void;
